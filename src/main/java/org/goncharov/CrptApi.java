fc12de1f4e35b148063e3d6e75cae836d8b136c3
Данил Гончаров <singlearity1@gmail.com>
2024-02-29 20:50:20 +0300

    first version

---
+++ src/main/java/org/goncharov/CrptApi.java
@@ -0,0 +1,283 @@
+package org.goncharov;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class CrptApi {
+    public static void main(String[] args) {
+        CrptApi rateLimiter = new CrptApi(TimeUnit.SECONDS, 2);
+        for (int i = 0; i < 10; i++) {
+            final int requestId = i;
+            rateLimiter.makeRequest("Request " + requestId);
+        }
+
+    }
+
+    private TimeUnit timeUnit;
+    //todo добавить валидацию, что число в рамках инт
+    private int requestLimit;
+
+    private Queue<String> requestQueue;
+    private ScheduledExecutorService scheduler;
+  //  private int processedRequests = 0;
+    public CrptApi(TimeUnit timeUnit, int requestLimit) {
+        this.timeUnit = timeUnit;
+        this.requestLimit = requestLimit;
+        this.requestQueue = new LinkedList<>();
+        this.scheduler = Executors.newScheduledThreadPool(1);
+        start();
+    }
+
+    public synchronized void makeRequest(String requestData) {
+        requestQueue.offer(requestData);
+        System.out.println("Request added to queue: " + requestData);
+
+    }
+
+    private void start() {
+        scheduler.scheduleAtFixedRate(this::processQueue, 0, 1, timeUnit);
+    }
+
+//    private void stop() {
+//        if (!scheduler.isShutdown()) {
+//            scheduler.shutdown();
+//        }
+//    }
+
+    private void processQueue() {
+        int processedRequests = 0;
+        while (!requestQueue.isEmpty() && processedRequests < requestLimit) {
+            processRequest(requestQueue.poll());
+            processedRequests++;
+        }
+       // stop();
+    }
+
+    private void processRequest(String requestData) {
+        System.out.println("Processing request: " + requestData);
+    }
+
+    public class DocumentDTO {
+        private Description description;
+        private String doc_id;
+        private String doc_status;
+        private String doc_type;
+        private boolean importRequest;
+        private String owner_inn;
+        private String participant_inn;
+        private String producer_inn;
+        private String production_date;
+        private String production_type;
+        private List<Product> products;
+        private String reg_date;
+        private String reg_number;
+
+
+        public Description getDescription() {
+            return description;
+        }
+
+        public void setDescription(Description description) {
+            this.description = description;
+        }
+
+        public String getDoc_id() {
+            return doc_id;
+        }
+
+        public void setDoc_id(String doc_id) {
+            this.doc_id = doc_id;
+        }
+
+        public String getDoc_status() {
+            return doc_status;
+        }
+
+        public void setDoc_status(String doc_status) {
+            this.doc_status = doc_status;
+        }
+
+        public String getDoc_type() {
+            return doc_type;
+        }
+
+        public void setDoc_type(String doc_type) {
+            this.doc_type = doc_type;
+        }
+
+        public boolean isImportRequest() {
+            return importRequest;
+        }
+
+        public void setImportRequest(boolean importRequest) {
+            this.importRequest = importRequest;
+        }
+
+        public String getOwner_inn() {
+            return owner_inn;
+        }
+
+        public void setOwner_inn(String owner_inn) {
+            this.owner_inn = owner_inn;
+        }
+
+        public String getParticipant_inn() {
+            return participant_inn;
+        }
+
+        public void setParticipant_inn(String participant_inn) {
+            this.participant_inn = participant_inn;
+        }
+
+        public String getProducer_inn() {
+            return producer_inn;
+        }
+
+        public void setProducer_inn(String producer_inn) {
+            this.producer_inn = producer_inn;
+        }
+
+        public String getProduction_date() {
+            return production_date;
+        }
+
+        public void setProduction_date(String production_date) {
+            this.production_date = production_date;
+        }
+
+        public String getProduction_type() {
+            return production_type;
+        }
+
+        public void setProduction_type(String production_type) {
+            this.production_type = production_type;
+        }
+
+        public List<Product> getProducts() {
+            return products;
+        }
+
+        public void setProducts(List<Product> products) {
+            this.products = products;
+        }
+
+        public String getReg_date() {
+            return reg_date;
+        }
+
+        public void setReg_date(String reg_date) {
+            this.reg_date = reg_date;
+        }
+
+        public String getReg_number() {
+            return reg_number;
+        }
+
+        public void setReg_number(String reg_number) {
+            this.reg_number = reg_number;
+        }
+    }
+
+    class Description {
+        private String participantInn;
+
+
+        public String getParticipantInn() {
+            return participantInn;
+        }
+
+        public void setParticipantInn(String participantInn) {
+            this.participantInn = participantInn;
+        }
+    }
+
+    class Product {
+        private String certificate_document;
+        private String certificate_document_date;
+        private String certificate_document_number;
+        private String owner_inn;
+        private String producer_inn;
+        private String production_date;
+        private String tnved_code;
+        private String uit_code;
+        private String uitu_code;
+
+
+        public String getCertificate_document() {
+            return certificate_document;
+        }
+
+        public void setCertificate_document(String certificate_document) {
+            this.certificate_document = certificate_document;
+        }
+
+        public String getCertificate_document_date() {
+            return certificate_document_date;
+        }
+
+        public void setCertificate_document_date(String certificate_document_date) {
+            this.certificate_document_date = certificate_document_date;
+        }
+
+        public String getCertificate_document_number() {
+            return certificate_document_number;
+        }
+
+        public void setCertificate_document_number(String certificate_document_number) {
+            this.certificate_document_number = certificate_document_number;
+        }
+
+        public String getOwner_inn() {
+            return owner_inn;
+        }
+
+        public void setOwner_inn(String owner_inn) {
+            this.owner_inn = owner_inn;
+        }
+
+        public String getProducer_inn() {
+            return producer_inn;
+        }
+
+        public void setProducer_inn(String producer_inn) {
+            this.producer_inn = producer_inn;
+        }
+
+        public String getProduction_date() {
+            return production_date;
+        }
+
+        public void setProduction_date(String production_date) {
+            this.production_date = production_date;
+        }
+
+        public String getTnved_code() {
+            return tnved_code;
+        }
+
+        public void setTnved_code(String tnved_code) {
+            this.tnved_code = tnved_code;
+        }
+
+        public String getUit_code() {
+            return uit_code;
+        }
+
+        public void setUit_code(String uit_code) {
+            this.uit_code = uit_code;
+        }
+
+        public String getUitu_code() {
+            return uitu_code;
+        }
+
+        public void setUitu_code(String uitu_code) {
+            this.uitu_code = uitu_code;
+        }
+    }
+
+}
